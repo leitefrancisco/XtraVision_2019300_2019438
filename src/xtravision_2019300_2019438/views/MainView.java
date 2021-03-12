b21286fa5879782da3f67e179f626b49b63acbf3
leitefrancisco <xicomeirelles94@gmail.com>
2021-03-12 13:09:19 -0300

    test views

--- src/xtravision_2019300_2019438/views/MainView.java
+++ src/xtravision_2019300_2019438/views/MainView.java
@@ -20,6 +20,7 @@ public class MainView extends javax.swing.JFrame {
         this.setSize(1280,720);
         this.setLocationRelativeTo(null);
         this.setResizable(false);
+        this.pack();
     }
 
     /**
@@ -31,7 +32,19 @@ public class MainView extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
         rentBtn = new javax.swing.JButton();
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("XtraVision");
@@ -107,6 +120,7 @@ public class MainView extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JButton rentBtn;
     // End of variables declaration//GEN-END:variables
 }
